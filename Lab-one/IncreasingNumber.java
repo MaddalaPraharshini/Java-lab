0cfa37b26e1d719eb988b2627700d3c0d2974845
MaddalaPraharshini <66952762+MaddalaPraharshini@users.noreply.github.com>
2021-02-14 22:51:15 +0530

    Lab one!!!!

---
+++ Lab-one/IncreasingNumber.java
@@ -0,0 +1,28 @@
+package day3;
+class checkNumber{
+	public boolean checker(int number) {
+		int num=0;
+		boolean flag=false;
+		num=number%10;
+		while(number!=0) {
+			if((number%10)<num) {
+				flag = true;
+				return flag;
+			}
+			number = number/10;
+		}
+		return flag;
+	}
+}
+
+
+
+public class IncreasingNumber{
+
+	public static void main(String[] args) {
+		checkNumber obj = new checkNumber();
+		System.out.println(obj.checker(12345));
+
+	}
+
+}
